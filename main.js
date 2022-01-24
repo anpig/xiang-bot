a7ec2b71f0f59bf26be9566696b9f5cb28e09114
Andrew Wang <pkid0anpig@gmail.com>
2022-01-24 16:01:59 +0800

    update to discord.js v13, this bot now can act on multi servers

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
 const auth = require("./auth.json");
-const Anpig = require("./Anpig.json");
-var response = require("./response.json");
-var muteroleid = "841734850872344597";
-bot.login(auth.BOT_TOKEN);
-bot.on("message", msg => {
-    var role = msg.member.guild.roles.cache.find(role => role.name === "醜狗閉嘴");
+const response = require("./response.json");
+const {Client, Intents} = require("discord.js");
+const bot = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+bot.login(auth.token);
+bot.on("messageCreate", msg => {
+    var user = msg.member;
+    var muterole = msg.member.guild.roles.cache.find(role => role.name === "醜狗閉嘴");
     if (msg.content == "醜狗閉嘴") {
-        if (msg.member.roles.cache.has(muteroleid)) {
+        if (user.roles.cache.has(muterole.id)) {
             msg.channel.send("醜狗已經閉嘴了唷");
         }
         else {
             msg.channel.send("現在開始醜狗不會回你話了唷");
-            msg.guild.members.cache.get(msg.author.id).roles.add(role);
+            user.roles.add(muterole).catch(console.error);
         }
     }
-    if (msg.content == "醜狗講話") {
-        if (msg.member.roles.cache.has(muteroleid)) {
+    else if (msg.content == "醜狗講話") {
+        if (msg.member.roles.cache.has(muterole.id)) {
             msg.channel.send("現在開始醜狗會回你話了唷");
-            msg.guild.members.cache.get(msg.author.id).roles.remove(role);
+            user.roles.remove(muterole).catch(console.error);
         }
         else {
             msg.channel.send("醜狗已經會回你了唷");
         }
     }
-    if (msg.content == "醜狗測試") {
-        if (msg.author.id == Anpig.id) {
-            msg.channel.send("醜狗在線上唷");
-        }
-    }
-    if (!msg.member.roles.cache.has(muteroleid)) {
+    else if (!msg.member.roles.cache.has(muterole.id)) {
         for (key in response) {
             if (msg.content == key) {
                 msg.channel.send(response[key]);
